2bb0d2ff0ffff72585df9dd8433257dcaec6201d
satyam <mishrasatyam9890@gmail.com>
2025-09-04 21:01:39 +0530

    Small Home UI

--- Collabio/lib/pages/home.dart
+++ Collabio/lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:frontend/widgets/Uihelper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,8 +8,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home'),
+      appBar: AppBar(
+        centerTitle: true,
+        leading: Center(child: FaIcon(FontAwesomeIcons.search)),
+        title: Uihelper.customTxt(text: 'Home'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FaIcon(FontAwesomeIcons.bell, size: 30),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Uihelper.customTxt(text: 'Suggestions', fontSize: 20),
+            SizedBox(height: 10,),
+            Container(
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.amber,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsetsGeometry.all(10),
+                          child: Uihelper.customImg(
+                            img: 'logo.png',
+                            height: 80,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Uihelper.customTxt(
+                        text: 'Satyam',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
