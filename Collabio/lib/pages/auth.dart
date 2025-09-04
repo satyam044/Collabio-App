de7d963b95d2290ec693293fd600c199def05769
satyam <mishrasatyam9890@gmail.com>
2025-09-04 20:45:38 +0530

    Auth Page  UI

--- Collabio/lib/pages/auth.dart
+++ Collabio/lib/pages/auth.dart
@@ -15,97 +15,102 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              icon: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Uihelper.customImg(img: 'logo.png', height: 40),
-                      SizedBox(width: 8),
-                      Uihelper.customTxt(
-                        text: 'Collabio',
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ],
+                  Uihelper.customTxt(
+                    text: 'Skip',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
                   ),
-                  SizedBox(height: 50),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Uihelper.customTxt(
-                        text: 'Together,',
-                        fontSize: 50,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      Uihelper.customTxt(
-                        text: 'we can achieve more',
-                        fontSize: 42,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ],
-                  ),
+                  SizedBox(width: 10),
+                  FaIcon(FontAwesomeIcons.arrowRight, color: Colors.grey),
                 ],
               ),
-              Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _onPhoneSignUpPressed(context),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      backgroundColor: Colors.green,
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      elevation: 5,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.phone,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                        SizedBox(width: 10),
-                        Uihelper.customTxt(
-                          text: 'Sign Up with Phone',
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 40),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              Scaffold(body: Center(child: Text("Home Page"))),
-                        ),
-                      );
-                    },
-                    child: Uihelper.customTxt(
-                      text: 'Already have an account? Log In',
-                      fontSize: 16,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 60),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Uihelper.customImg(img: 'logo.png', height: 40),
+                    SizedBox(width: 8),
+                    Uihelper.customTxt(
+                      text: 'Collabio',
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 50),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Uihelper.customTxt(
+                      text: 'Together,',
+                      fontSize: 50,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    Uihelper.customTxt(
+                      text: 'we can achieve more',
+                      fontSize: 42,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () => _onPhoneSignUpPressed(context),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.green,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                elevation: 5,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.phone,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  SizedBox(width: 10),
+                  Uihelper.customTxt(
+                    text: 'Sign Up with Phone',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
