943768b57e63f9b8d499b81a977dc7ed37eeb1ee
satyam <mishrasatyam9890@gmail.com>
2025-09-06 15:46:12 +0530

    Chat Page UI

--- Collabio/lib/pages/chat.dart
+++ Collabio/lib/pages/chat.dart
@@ -6,9 +6,12 @@ class ChatPage extends StatelessWidget {
   final int user;
   ChatPage(this.user);
 
+  final TextEditingController _messageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF0F2027),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         elevation: 0,
@@ -68,6 +71,76 @@ class ChatPage extends StatelessWidget {
             onPressed: () {},
           ),
           SizedBox(width: 8),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.all(12),
+              itemCount: 20,
+              separatorBuilder: (_, __) => SizedBox(height: 8),
+              itemBuilder: (context, index) {
+                bool isMe = index % 2 == 0;
+                return Align(
+                  alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isMe ? Colors.blue[100] : Colors.grey[200],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Uihelper.customTxt(
+                      text: "Message ${index + 1}",
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Divider(height: 1),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            color: Colors.white,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: "Type a message",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[100],
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                CircleAvatar(
+                  radius: 22,
+                  backgroundColor: Colors.blue,
+                  child: IconButton(
+                    icon: FaIcon(FontAwesomeIcons.chevronRight, color: Colors.white),
+                    onPressed: () {
+                      String message = _messageController.text.trim();
+                      if (message.isNotEmpty) {
+                        print("Sending: $message");
+                        _messageController.clear();
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
